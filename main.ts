9ec9db3d7ceff33d74a358a8b3aa173e89f95379
Jupegarnica <juan@geekshubs.com>
2022-09-19 09:59:17 +0200

    chore: clean up

--- main.ts
+++ main.ts
@@ -1,34 +1,20 @@
-// import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 import { config } from "https://deno.land/std@0.156.0/dotenv/mod.ts?s=config";
-// const args = Deno.args
 
 export async function loadEnv() {
   const env = await config();
-
-  // for (const key in env) {
-  //   Deno.env.set(key, env[key]);
-  //   // console.log(key, env[key]);
-
-  // }
-
-  // const [command, ...args] = Deno.args
-  // const { code, stdout, stderr } = Deno.spawnSync(command, { args, env, clearEnv: false });
-
-  // const args = ['-c', ...Deno.args]
   const args = ['-c', Deno.args.join(' ')]
-  // Deno.spawnSync('sh', { args, env, clearEnv: false });
-  // console.log({args});
 
 
   const { code, stdout, stderr } = Deno.spawnSync('sh', { args, env, clearEnv: false, cwd: Deno.cwd() });
   const output = new TextDecoder().decode(stdout);
   console.log(output);
-
-  // const error = new TextDecoder().decode(stderr);
-  // console.log({error, code});
-
-
-
+  const error = new TextDecoder().decode(stderr);
+  if (error) {
+    console.error(error);
+  }
+  if (code !== 0) {
+    Deno.exit(code);
+  }
 }
 
 await loadEnv();
